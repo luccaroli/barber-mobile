e92d49b260d81f2d05d20a2e3f2956fbe1721210
luccaroli <luccaroli20@gmail.com>
2020-10-04 12:10:09 -0300

    refactor: page sign finish

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,4 +1,5 @@
-import styled from 'styled-components/native';
+import styled from 'styled-components/native'
+import { getBottomSpace } from 'react-native-iphone-x-helper'
 
 export const Container = styled.View`
   flex: 1;
@@ -13,4 +14,35 @@ export const Title = styled.Text`
   color: #f4ede4;
   font-family: 'Ubuntu_500Medium';
   margin: 32px 0 24px;
+`
+
+export const ForgotPassword = styled.TouchableOpacity`
+  margin-top: 24px;
+`
+
+export const ForgotPasswordText = styled.Text`
+  color: #f4ede4;
+  font-size: 14px;
+  font-family: 'Ubuntu_400Regular';
+`
+
+export const CreateAccountButton = styled.TouchableOpacity`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  border-top-width: 1px;
+  border-color: #232129;
+  padding: 16px 0 ${14 + getBottomSpace()}px;
+
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`
+
+export const CreateAccountButtonText = styled.Text`
+  color: #ff9000;
+  font-size: 14px;
+  font-family: 'Ubuntu_400Regular';
+  margin-left: 8px;
 `
